be2e142cf7edc929408531cea70545e81067ae0a
MuskanNaaz <muskannaaz1999@gmail.com>
2020-07-11 23:10:45 +0530

    Adding Array functionality

---
+++ src/Sdata.js
@@ -0,0 +1,39 @@
+const sdata=[
+    {
+        imgsrc:"https://www.gstatic.com/tv/thumb/v22vodart/14664464/p14664464_v_v8_aa.jpg",
+        title:"A Netflix original Series",
+        sname:"dark",
+        link:"https://www.youtube.com/watch?v=J_yb8HORges",
+    },
+    {
+        imgsrc:"https://cdn.bollywoodmdb.com/movies/largethumb/2019/hindi-medium-2/poster.jpg",
+        title:"A Netflix original Series",
+        sname:"Angrezi Medium",
+        link:"https://www.bollywoodmdb.com/movies/broadband/angrezi-medium-official-trailer",
+    },
+    {
+        imgsrc:"https://cdn.bollywoodmdb.com/movies/largethumb/2019/baaghi-3/baaghi-3-poster-7.jpg",
+        title:"A Netflix original Series",
+        sname:"Baaghi-3",
+        link:"https://www.bollywoodmdb.com/movies/broadband/baaghi-3-official-trailer",
+    },
+    {
+        imgsrc:"https://cdn.bollywoodmdb.com/movies/largethumb/2020/thappad/poster.jpg",
+        title:"A Netflix original Series",
+        sname:"Thappad",
+        link:"https://www.bollywoodmdb.com/movies/broadband/thappad-official-trailer",
+    },
+    {
+        imgsrc:"https://cdn.bollywoodmdb.com/movies/largethumb/2020/doordarshan/poster.jpg",
+        title:"A Netflix original Series",
+        sname:"dark",
+        link:"https://www.bollywoodmdb.com/movies/broadband/doordarshan-official-trailer",
+    },
+    {
+        imgsrc:"https://cdn.bollywoodmdb.com/movies/largethumb/2019/gulabo-sitabo/poster.jpg",
+        title:"A Netflix original Series",
+        sname:"Gulabo Sitabo",
+        link:"https://www.bollywoodmdb.com/movies/broadband/gulabo-sitabo-official-trailer-2",
+    },
+];
+export default sdata;
--- src/index.js
+++ src/index.js
@@ -4,41 +4,41 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Card from "./Cards";
+import sdata from "./Sdata";
+
 ReactDOM.render( 
 <>
 <h1 className="heading_style">List of Top 6 Netflix bollywood Series 2020</h1>
 
-<Card imgsrc="https://www.gstatic.com/tv/thumb/v22vodart/14664464/p14664464_v_v8_aa.jpg"
-title="A Netflix original Series"
-sname="dark"
-link="https://www.youtube.com/watch?v=J_yb8HORges"/>
+<Card imgsrc={sdata[0].imgsrc}
+title={sdata[0].title}
+sname={sdata[0].sname}
+link={sdata[0].link}/>
 
+<Card imgsrc={sdata[1].imgsrc}
+title={sdata[1].title}
+sname={sdata[1].sname}
+link={sdata[1].link}/>
 
-<Card imgsrc="https://cdn.bollywoodmdb.com/movies/largethumb/2019/hindi-medium-2/poster.jpg"
-title="A Netflix original Series"
-sname="Angrezi Medium"
-link="https://www.bollywoodmdb.com/movies/broadband/angrezi-medium-official-trailer"/>
+<Card imgsrc={sdata[2].imgsrc}
+title={sdata[2].title}
+sname={sdata[2].sname}
+link={sdata[2].link}/>
 
-<Card imgsrc="https://cdn.bollywoodmdb.com/movies/largethumb/2019/baaghi-3/baaghi-3-poster-7.jpg"
-title="A Netflix original Series"
-sname="Baaghi-3"
-link="https://www.bollywoodmdb.com/movies/broadband/baaghi-3-official-trailer"/>
+<Card imgsrc={sdata[3].imgsrc}
+title={sdata[3].title}
+sname={sdata[3].sname}
+link={sdata[3].link}/>
 
-<Card imgsrc="https://cdn.bollywoodmdb.com/movies/largethumb/2020/thappad/poster.jpg"
-title="A Netflix original Series"
-sname="Thappad"
-link="https://www.bollywoodmdb.com/movies/broadband/thappad-official-trailer"/>
+<Card imgsrc={sdata[4].imgsrc}
+title={sdata[4].title}
+sname={sdata[4].sname}
+link={sdata[4].link}/>
 
-<Card imgsrc="https://cdn.bollywoodmdb.com/movies/largethumb/2020/doordarshan/poster.jpg"
-title="A Netflix original Series"
-sname="dark"
-link="https://www.bollywoodmdb.com/movies/broadband/doordarshan-official-trailer"/>
-
-<Card imgsrc="https://cdn.bollywoodmdb.com/movies/largethumb/2019/gulabo-sitabo/poster.jpg"
-title="A Netflix original Series"
-sname="Gulabo Sitabo"
-link="https://www.bollywoodmdb.com/movies/broadband/gulabo-sitabo-official-trailer-2"/>
-
+<Card imgsrc={sdata[5].imgsrc}
+title={sdata[5].title}
+sname={sdata[5].sname}
+link={sdata[5].link}/>
 </>,
   document.getElementById('root')
 );
