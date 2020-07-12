08aab00461cda8c31161b1a18a377481eb41a015
MuskanNaaz <muskannaaz1999@gmail.com>
2020-07-12 19:37:40 +0530

    Adding map and fat function

--- src/index.js
+++ src/index.js
@@ -6,39 +6,31 @@ import * as serviceWorker from './serviceWorker';
 import Card from "./Cards";
 import sdata from "./Sdata";
 
+// creating Map function
+// function netcard(val){
+//   return(
+//     <Card 
+//     imgsrc={val.imgsrc}
+//     title={val.title}
+//     sname={val.sname}
+//     link={val.link}/>
+//   )
+// }
+
 ReactDOM.render( 
 <>
 <h1 className="heading_style">List of Top 6 Netflix bollywood Series 2020</h1>
-
-<Card imgsrc={sdata[0].imgsrc}
-title={sdata[0].title}
-sname={sdata[0].sname}
-link={sdata[0].link}/>
-
-<Card imgsrc={sdata[1].imgsrc}
-title={sdata[1].title}
-sname={sdata[1].sname}
-link={sdata[1].link}/>
-
-<Card imgsrc={sdata[2].imgsrc}
-title={sdata[2].title}
-sname={sdata[2].sname}
-link={sdata[2].link}/>
-
-<Card imgsrc={sdata[3].imgsrc}
-title={sdata[3].title}
-sname={sdata[3].sname}
-link={sdata[3].link}/>
-
-<Card imgsrc={sdata[4].imgsrc}
-title={sdata[4].title}
-sname={sdata[4].sname}
-link={sdata[4].link}/>
-
-<Card imgsrc={sdata[5].imgsrc}
-title={sdata[5].title}
-sname={sdata[5].sname}
-link={sdata[5].link}/>
+{/* {sdata.map(netcard)} */}
+{/* using Fat arrow method to array the data => */}
+{sdata.map((val,index)=>{
+return(
+  <Card 
+    imgsrc={val.imgsrc}
+    title={val.title}
+     sname={val.sname}
+     link={val.link}/>
+    );
+})}
 </>,
   document.getElementById('root')
 );
